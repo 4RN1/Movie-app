2d19adaf2c389663c884bdac01861d4f937aafca
Anri Begheladze <anriskr14@gmail.com>
2025-09-04 19:21:19 +0400

    intial commit

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './css/App.css'
+import MovieCard from './components/MovieCard'
+import Favorites from './pages/Favorites';
+import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import { MovieProvider } from './context/MovieContext';
+
+function App() {
+
+
+  return (
+    <>
+    <MovieProvider>
+      <Navbar/>
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/favorites' element={<Favorites />}/>
+    
+        </Routes>
+      </main>
+      </MovieProvider>
+    </>
+  );
+}
+
+
+
+
+export default App
